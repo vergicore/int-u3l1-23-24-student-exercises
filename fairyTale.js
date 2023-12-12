42a31a7cb8ee175cb49fc34cff0a146250774f4a
vergicore <147555888+vergicore@users.noreply.github.com>
2023-12-12 22:02:52 +0000

    learned basics of JS

--- fairyTale.js
+++ fairyTale.js
@@ -1,27 +1,27 @@
 // 1. Create a variable named `place` and set the value to a string that says whatever place you want.
-
+let place = "Japanese Village";
 // 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
-
+let creatureName = "Luna";
 // 3. Create a variable named `color` and set the value to a string that says whatever color you want.
-
+let color = "green";
 // 4. Create a variable named `color2` and set the value to a string that says a different color you want.
-
+let color2 = "blue";
 // 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
-
+let emotion = "tired";
 // 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
-
+let character = "Bob";
 // 7. Create a variable named `number` and set the value to a number greater than 1.
-
+let number = 235678;
 // 8. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
-
+let item = "Uranium";
 // 9. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
-
+let item2 = "piece of Iodine";
 // 10. Create a variable named `verb` and set the value to a string that says whatever verb you want.
-
+let verb = "danc";
 // 11. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
-
+let adverb = "rapidly";
 // 12. Create a variable named `number2` and set the value to a number greater than 1.
-
+let number2 = 21;
 
 
 
@@ -31,19 +31,19 @@
 // - Afterwards, run your code and press the button to see your story ✨
 
 document.querySelector("button").addEventListener("click", function() {
-    document.querySelector(".place").innerHTML = null;
-    document.querySelector(".creature-name").innerHTML = null;
-    document.querySelector(".color").innerHTML = null;
-    document.querySelector(".color-2").innerHTML = null;
-    document.querySelector(".emotion").innerHTML = null;
-    document.querySelector(".character").innerHTML = null;
-    document.querySelector(".creature-name-again").innerHTML = null;
-    document.querySelector(".number").innerHTML = null;
-    document.querySelector(".item").innerHTML = null;
-    document.querySelector(".character-again").innerHTML = null;
-    document.querySelector(".item-2").innerHTML = null;
-    document.querySelector(".verb").innerHTML = null;
-    document.querySelector(".adverb").innerHTML = null;
-    document.querySelector(".number-2").innerHTML = null;
+    document.querySelector(".place").innerHTML = place;
+    document.querySelector(".creature-name").innerHTML = creatureName;
+    document.querySelector(".color").innerHTML = color;
+    document.querySelector(".color-2").innerHTML = color2;
+    document.querySelector(".emotion").innerHTML = emotion;
+    document.querySelector(".character").innerHTML = character;
+    document.querySelector(".creature-name-again").innerHTML = creatureName;
+    document.querySelector(".number").innerHTML = number;
+    document.querySelector(".item").innerHTML = item;
+    document.querySelector(".character-again").innerHTML = character;
+    document.querySelector(".item-2").innerHTML = item2;
+    document.querySelector(".verb").innerHTML = verb;
+    document.querySelector(".adverb").innerHTML = adverb;
+    document.querySelector(".number-2").innerHTML = number2;
     document.querySelector(".story-container").style = "display:block";
 });
--- variables.js
+++ variables.js
@@ -2,17 +2,22 @@
 let favoriteColor;
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood;
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber;
 
 
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
 // - Use a string value of your favorite color.
+favoriteColor = "green"
 // - Use a string value of your favorite food.
+favoriteFood = "ramen"
 // - Use a number value of your favorite number.
-
+favoriteNumber = 21
 // 4. Console log each variable on a new line.
+console.log(favoriteColor);
+console.log(favoriteFood);
+console.log(favoriteNumber);
